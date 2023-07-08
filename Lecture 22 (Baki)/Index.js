d9912ecff02a6cc13e8c86749ac2aa9885345ba6
Jaymin Darji <jaymindarji2003@gmail.com>
2023-07-08 19:01:33 +0530

    Done with the remaing task

--- Lecture 22 (Baki)/Index.js
+++ Lecture 22 (Baki)/Index.js
@@ -1,26 +1,28 @@
-// /guess the number
+// Exercise 1 :- Guess the number
 let Input;
 let a = Math.floor(Math.random() * 100);
-let flag = 0;
+let count = 0;
+
 do {
   Input = prompt("Enter the number");
+
+  // case 1
   if (Input == a) {
     console.log("Its match");
     break;
-  } else if (a < Input) {
-    console.log("Its actual nummber less than", "It requires more number "); //);
-    flag++;
-  } else {
-    console.log("Its actual nummber graeter than", "It enough number "); // a - Input);
-    flag++;
+  } 
+  
+  // case 2
+  else if (a < Input) {
+    console.log("Enter Smaller number");
+    count++;
+  }
+  
+  // case 3
+  else {
+    console.log("Enter Greater number")
+    count++;
   }
 } while (a != Input);
-if (flag == 0) console.log(" Its an outstanding performance");
-else if (flag < 10) console.log("You can score 50 points ");
-else if (flag == 10) console.log("You can score 90 points");
-else if (flag > 10 && flag != 100) console.log("You can score 95 points");
-else if (flag == 100) {
-  console.log("You cant play again");
-} else {
-  console.log("Default value");
-}
+
+console.log("Your score : ", 100-count , "%");
