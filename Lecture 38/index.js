92549c969dd7ebd62219a63730431a421a0d074a
Jaymin Darji <jaymindarji2003@gmail.com>
2023-07-20 23:01:45 +0530

    done with lecture

---
+++ Lecture 38/index.js
@@ -0,0 +1,5 @@
+// // Q.1
+// document.getElementsByTagName("nav")[0].firstElementChild.firstElementChild.style.color = "red";
+
+document.getElementsByTagName("nav")[0].firstElementChild.firstElementChild.style.color = "green";
+document.getElementsByTagName("nav")[0].firstElementChild.lastElementChild.style.color = "green";
